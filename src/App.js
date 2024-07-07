8a68f46179fa7deb584b2ae8b25bacbf8a930c26
Ilya Kabytov <kabytovi@gmail.com>
2024-07-07 14:43:44 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ import {
 } from 'react-router-dom';
 
 import {Covid} from './Covid';
-import {Tub} from './Tub';
 import {Main} from './Main';
 import {Modeling} from './Modeling';
 import {Data} from './Data';
@@ -63,7 +62,6 @@ return (<>
        </Helmet>
           <Routes>
             <Route exact path="/statistics" element={<Covid />} />
-            <Route exact path="/tub" element={<Tub />} />
             <Route exact path="/" element={<Main />} />
             <Route exact path="/modeling" element={<Modeling />} />
             <Route exact path="/data" element={<Data />} />
