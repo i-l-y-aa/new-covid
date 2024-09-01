fba1d39a4c2dc04e906d6c00975e59148133d455
Ilya Kabytov <kabytovi@gmail.com>
2024-09-01 18:55:28 +0700

    fix

--- src/Tub.js
+++ src/Tub.js
@@ -41,6 +41,28 @@ const regions = [
   getRegionData(18, "https://server.covid19-modeling.ru/api/csvTub/kareliya", "kareliya", "Республика Карелия", "Республика Карелия республика карелия","Северо-Западный федеральный округ","Петразаводск", "523 856 ч.", "172 400 км²", "2,9 чел./км²"),
   getRegionData(19, "https://server.covid19-modeling.ru/api/csvTub/komi", "komi", "Республика Коми", "Республика Коми республика коми","Северо-Западный федеральный округ","Сыктывкар", "720 610 ч.", "415 900 км²", "1,73 чел./км²"),
   getRegionData(20, "https://server.covid19-modeling.ru/api/csvTub/arhangelsk", "arhangelsk", "Архангельская область", "Архангельская область архангельская область","Северо-Западный федеральный округ","Архангельск", "998 072 ч.", "587 400 км²", "1,69 чел./км²"),
+  getRegionData(21, "https://server.covid19-modeling.ru/api/csvTub/vologda", "vologda", "Вологодская область", "Вологодская область вологодская область","Северо-Западный федеральный округ","Вологда", "1 121 343 ч.", "144 527 км²", "8,5 чел./км²"),
+  getRegionData(22, "https://server.covid19-modeling.ru/api/csvTub/kaliningrad", "kaliningrad", "Калининградская область", "Калининградская область калининградская область","Северо-Западный федеральный округ","Калининград", "1 033 914 ч.", "15 125 км²", "68,36 чел./км²"),
+  getRegionData(23, "https://server.covid19-modeling.ru/api/csvTub/leningrad", "leningrad", "Ленинградская область", "Ленинградская область ленинградская область","Северо-Западный федеральный округ","Гатчина", "2 035 762 ч.", "83 908 км²", "24,26 чел./км²"),
+  getRegionData(24, "https://server.covid19-modeling.ru/api/csvTub/murmansk", "murmansk", "Мурманская область", "Мурманская область мурманская область","Северо-Западный федеральный округ","Мурманск", "656 438 ч.", "144 902 км²", "4,53 чел./км²"),
+  getRegionData(25, "https://server.covid19-modeling.ru/api/csvTub/novgorod", "novgorod", "Новгородская область", "Новгородская область новгородская область","Северо-Западный федеральный округ","Великий Новгород", "571 447 ч.", "54 501 км²", "10,49 чел./км²"),
+  getRegionData(26, "https://server.covid19-modeling.ru/api/csvTub/pskov", "pskov", "Псковская область", "Псковская область псковская область","Северо-Западный федеральный округ","Псков", "581 147 ч.", "55 399 км²", "10,49 чел./км²"),
+  getRegionData(27, "https://server.covid19-modeling.ru/api/csvTub/adygea", "adygea", "Республика Адыгея", "Республика Адыгея республика адыгея","Южный федеральный округ","Майкоп", "500 591 ч.", "7 792 км²", "64,24 чел./км²"),
+  getRegionData(28, "https://server.covid19-modeling.ru/api/csvTub/kalmykia", "kalmykia", "Республика Калмыкия", "Республика Калмыкия республика калмыкия","Южный федеральный округ","Элиста", "266 770 ч.", "74 731 км²", "5,57 чел./км²"),
+  getRegionData(29, "https://server.covid19-modeling.ru/api/csvTub/krasnodar", "krasnodar", "Краснодарский край", "Краснодарский край краснодарский край","Южный федеральный округ","Краснодар", "5 833 002 ч.", "76 005 км²", "77,27 чел./км²"),
+  getRegionData(30, "https://server.covid19-modeling.ru/api/csvTub/volgograd", "volgograd", "Волгоградская область", "Волгоградская область волгоградская область","Южный федеральный округ","Волгоград", "2 453 898 ч.", "113 900 км²", "21,74 чел./км²"),
+  getRegionData(31, "https://server.covid19-modeling.ru/api/csvTub/rostov", "rostov", "Ростовская область", "Ростовская область ростовская область","Южный федеральный округ","Ростов", "4 152 518 ч.", "100 967 км²", "41,13 чел./км²"),
+  getRegionData(32, "https://server.covid19-modeling.ru/api/csvTub/dagestan", "dagestan", "Республика Дагестан", "Республика Дагестан республика дагестан","Южный федеральный округ","Махачкала", "3 232 224 ч.", "50 270 км²", "64,3 чел./км²"),
+  getRegionData(33, "https://server.covid19-modeling.ru/api/csvTub/ingushetia", "ingushetia", "Республика Ингушетия", "Республика Ингушетия республика ингушетия","Южный федеральный округ","Магас", "509 541 ч.", "3 685 км²", "147,7 чел./км²"),
+  getRegionData(34, "https://server.covid19-modeling.ru/api/csvTub/kabarbalkar", "kabarbalkar", "Кабардино-Балкарская Республика", "Кабардино-Балкарская Республика кабардино-балкарская республик","Южный федеральный округ","Нальчик", "905 464 ч.", "76 005 км²", "72,61 чел./км²"),
+  getRegionData(35, "https://server.covid19-modeling.ru/api/csvTub/karcherk", "karcherk", "Карачаево-Черкесская Республика", "Карачаево-Черкесская Республика карачаево-черкесская республика","Южный федеральный округ","Черкесск", "468 322 ч.", "14 100 км²", "32,8 чел./км²"),
+  getRegionData(36, "https://server.covid19-modeling.ru/api/csvTub/sevosetiaalania", "sevosetiaalania", "Республика Северная Осетия-Алания", "Республика Северная Осетия-Алания республика северная осетия-алания","Северо-Кавказский федеральный округ","Владикавказ", "678 879 ч.", "8 000 км²", "85,00 чел./км²"),
+  getRegionData(37, "https://server.covid19-modeling.ru/api/csvTub/chechnya", "chechnya", "Чеченская республика", "Чеченская республика чеченская республика","Северо-Кавказский федеральный округ","Грозный", "1 552 866 ч.", "16 171 км²", "96,03 чел./км²"),
+  getRegionData(38, "https://server.covid19-modeling.ru/api/csvTub/stavrapol", "stavrapol", "Ставропольский край", "Ставропольский край ставропольский край","Северо-Кавказский федеральный округ","Ставрополь", "2 886 108 ч.", "66 160 км²", "43,62 чел./км²"),
+  getRegionData(39, "https://server.covid19-modeling.ru/api/csvTub/bashkortostan", "bashkortostan", "Республика Башкортостан", "Республика Башкортастан республика башкортастан","Приволжский федеральный округ","Уфа", "4 064 361 ч.", "142 947 км²", "28,4 чел./км²"),
+  getRegionData(40, "https://server.covid19-modeling.ru/api/csvTub/maryal", "maryal", "Республика Марий Эл", "Республика Марий Эл республика марий эл","Приволжский федеральный округ","Йошкар-Ола", "696 459 ч.", "23 375 км²", "29,5 чел./км²"),
+  getRegionData(41, "https://server.covid19-modeling.ru/api/csvTub/mordovia", "mordovia", "Республика Мордовия", "Республика Мордовия республика мордовия","Приволжский федеральный округ","Саранск", "765 891  ч.", "26 128 км²", "29,31 чел./км²"),
+  getRegionData(42, "https://server.covid19-modeling.ru/api/csvTub/tatarstan", "tatarstan", "Республика Татарстан", "Республика Татарстан республика татарстан","Приволжский федеральный округ","Казань", "4 003 016 ч.", "67 847 км²", "56,57 чел./км²"),
   
   getRegionData(21, "https://server.covid19-modeling.ru/api/csvTub/novosibirsk", "novosibirsk", "Новосибирская область", "Новосибирская область новосибирская область","Сибирский федеральный округ","Новосибирск", "2 779 375 ч.", "177 756 км²", "15,64 чел./км²"),
   getRegionData(22, "https://server.covid19-modeling.ru/api/csvTub/altay", "altay", "Алтайский край" , "Алтайский край алтайский край","Сибирский федеральный округ","Барнаул", "2 268 179 ч.", "167 996 км²", "13,5 чел./км²"),
@@ -69,6 +91,28 @@ const regionsSoc = [
   getRegionData(18, "https://server.covid19-modeling.ru/api/csvSocTub/kareliya", "kareliya", "Республика Карелия", "Республика Карелия республика карелия","Северо-Западный федеральный округ","Петразаводск", "523 856 ч.", "172 400 км²", "2,9 чел./км²"),
   getRegionData(19, "https://server.covid19-modeling.ru/api/csvSocTub/komi", "komi", "Республика Коми", "Республика Коми", "Республика Коми республика коми","Северо-Западный федеральный округ","Сыктывкар", "720 610 ч.", "415 900 км²", "1,73 чел./км²"),
   getRegionData(20, "https://server.covid19-modeling.ru/api/csvSocTub/arhangelsk", "arhangelsk", "Архангельская область", "Архангельская область архангельская область","Северо-Западный федеральный округ","Архангельск", "998 072 ч.", "587 400 км²", "1,69 чел./км²"),
+  getRegionData(21, "https://server.covid19-modeling.ru/api/csvSocTub/vologda", "vologda", "Вологодская область", "Вологодская область вологодская область","Северо-Западный федеральный округ","Вологда", "1 121 343 ч.", "144 527 км²", "8,5 чел./км²"),
+  getRegionData(22, "https://server.covid19-modeling.ru/api/csvSocTub/kaliningrad", "kaliningrad", "Калининградская область", "Калининградская область калининградская область","Северо-Западный федеральный округ","Калининград", "1 033 914 ч.", "15 125 км²", "68,36 чел./км²"),
+  getRegionData(23, "https://server.covid19-modeling.ru/api/csvSocTub/leningrad", "leningrad", "Ленинградская область", "Ленинградская область ленинградская область","Северо-Западный федеральный округ","Гатчина", "2 035 762 ч.", "83 908 км²", "24,26 чел./км²"),
+  getRegionData(24, "https://server.covid19-modeling.ru/api/csvSocTub/murmansk", "murmansk", "Мурманская область", "Мурманская область мурманская область","Северо-Западный федеральный округ","Мурманск", "656 438 ч.", "144 902 км²", "4,53 чел./км²"),
+  getRegionData(25, "https://server.covid19-modeling.ru/api/csvSocTub/novgorod", "novgorod", "Новгородская область", "Новгородская область новгородская область","Северо-Западный федеральный округ","Великий Новгород", "571 447 ч.", "54 501 км²", "10,49 чел./км²"),
+  getRegionData(26, "https://server.covid19-modeling.ru/api/csvSocTub/pskov", "pskov", "Псковская область", "Псковская область псковская область","Северо-Западный федеральный округ","Псков", "581 147 ч.", "55 399 км²", "10,49 чел./км²"),
+  getRegionData(27, "https://server.covid19-modeling.ru/api/csvSocTub/adygea", "adygea", "Республика Адыгея", "Республика Адыгея республика адыгея","Южный федеральный округ","Майкоп", "500 591 ч.", "7 792 км²", "64,24 чел./км²"),
+  getRegionData(28, "https://server.covid19-modeling.ru/api/csvSocTub/kalmykia", "kalmykia", "Республика Калмыкия", "Республика Калмыкия республика калмыкия","Южный федеральный округ","Элиста", "266 770 ч.", "74 731 км²", "5,57 чел./км²"),
+  getRegionData(29, "https://server.covid19-modeling.ru/api/csvSocTub/krasnodar", "krasnodar", "Краснодарский край", "Краснодарский край краснодарский край","Южный федеральный округ","Краснодар", "5 833 002 ч.", "76 005 км²", "77,27 чел./км²"),
+  getRegionData(30, "https://server.covid19-modeling.ru/api/csvSocTub/volgograd", "volgograd", "Волгоградская область", "Волгоградская область волгоградская область","Южный федеральный округ","Волгоград", "2 453 898 ч.", "113 900 км²", "21,74 чел./км²"),
+  getRegionData(31, "https://server.covid19-modeling.ru/api/csvSocTub/rostov", "rostov", "Ростовская область", "Ростовская область ростовская область","Южный федеральный округ","Ростов", "4 152 518 ч.", "100 967 км²", "41,13 чел./км²"),
+  getRegionData(32, "https://server.covid19-modeling.ru/api/csvSocTub/dagestan", "dagestan", "Республика Дагестан", "Республика Дагестан", "Республика Дагестан республика дагестан","Северо-Кавказский федеральный округ","Махачкала", "3 232 224 ч.", "50 270 км²", "64,3 чел./км²"),
+  getRegionData(33, "https://server.covid19-modeling.ru/api/csvSocTub/ingushetia", "ingushetia", "Республика Ингушетия", "Республика Ингушетия республика ингушетия","Северо-Кавказский федеральный округ","Магас", "509 541 ч.", "3 685 км²", "147,7 чел./км²"),
+  getRegionData(34, "https://server.covid19-modeling.ru/api/csvSocTub/kabarbalkar", "kabarbalkar", "Кабардино-Балкарская Республика", "Кабардино-Балкарская Республика кабардино-балкарская республик","Северо-Кавказский федеральный округ","Нальчик", "905 464 ч.", "76 005 км²", "72,61 чел./км²"),
+  getRegionData(35, "https://server.covid19-modeling.ru/api/csvSocTub/karcherk", "karcherk", "Карачаево-Черкесская Республика", "Карачаево-Черкесская Республика карачаево-черкесская республика","Северо-Кавказский федеральный округ","Черкесск", "468 322 ч.", "14 100 км²", "32,8 чел./км²"),
+  getRegionData(36, "https://server.covid19-modeling.ru/api/csvSocTub/sevosetiaalania", "sevosetiaalania", "Республика Северная Осетия-Алания", "Республика Северная Осетия-Алания республика северная осетия-алания","Северо-Кавказский федеральный округ","Владикавказ", "678 879 ч.", "8 000 км²", "85,00 чел./км²"),
+  getRegionData(37, "https://server.covid19-modeling.ru/api/csvSocTub/chechnya", "chechnya", "Чеченская республика", "Чеченская республика чеченская республика","Северо-Кавказский федеральный округ","Грозный", "1 552 866 ч.", "16 171 км²", "96,03 чел./км²"),
+  getRegionData(38, "https://server.covid19-modeling.ru/api/csvSocTub/stavrapol", "stavrapol", "Ставропольский край", "Ставропольский край ставропольский край","Северо-Кавказский федеральный округ","Ставрополь", "2 886 108 ч.", "66 160 км²", "43,62 чел./км²"),
+  getRegionData(39, "https://server.covid19-modeling.ru/api/csvSocTub/bashkortostan", "bashkortostan", "Республика Башкортостан", "Республика Башкортастан республика башкортастан","Приволжский федеральный округ","Уфа", "4 064 361 ч.", "142 947 км²", "28,4 чел./км²"),
+  getRegionData(40, "https://server.covid19-modeling.ru/api/csvSocTub/maryal", "maryal", "Республика Марий Эл", "Республика Марий Эл республика марий эл","Приволжский федеральный округ","Йошкар-Ола", "696 459 ч.", "23 375 км²", "29,5 чел./км²"),
+  getRegionData(41, "https://server.covid19-modeling.ru/api/csvSocTub/mordovia", "mordovia", "Республика Мордовия", "Республика Мордовия республика мордовия","Приволжский федеральный округ","Саранск", "765 891  ч.", "26 128 км²", "29,31 чел./км²"),
+  getRegionData(42, "https://server.covid19-modeling.ru/api/csvSocTub/tatarstan", "tatarstan", "Республика Татарстан", "Республика Татарстан республика татарстан","Приволжский федеральный округ","Казань", "4 003 016 ч.", "67 847 км²", "56,57 чел./км²"),
 
   getRegionData(21, "https://server.covid19-modeling.ru/api/csvSocTub/novosibirsk", "novosibirsk", "Новосибирская область", "Новосибирская область новосибирская область","Сибирский федеральный округ","Новосибирск", "2 779 375 ч.", "177 756 км²", "15,64 чел./км²"),
   getRegionData(22, "https://server.covid19-modeling.ru/api/csvSocTub/altay", "altay", "Алтайский край", "Алтайский край алтайский край","Сибирский федеральный округ","Барнаул", "2 268 179 ч.", "167 996 км²", "13,5 чел./км²"),
@@ -134,14 +178,15 @@ export function Tub() {
             </form>
           </motion.h3>
         </Container>
+        <Button className="my-2 all-region" onClick={() => setShowAllRegions(!showAllRegions)}>
+        {showAllRegions ? "Скрыть регионы" : "Показать все регионы"}
+      </Button>
         {noResults && (
           <div className="text-center text-danger my-3">
             По вашему запросу регион не найден
           </div>
         )}
-              <Button className="my-2 all-region" onClick={() => setShowAllRegions(!showAllRegions)}>
-        {showAllRegions ? "Скрыть регионы" : "Показать все регионы"}
-      </Button>
+              
         <Tab.Container id="left-tabs-example" defaultActiveKey={filteredRegions.length > 0 ? filteredRegions[0].eventKey : ""}>
           <div className="horizontal-scroll">
             <Nav variant="pills" defaultActiveKey="/home" className="d-flex">
