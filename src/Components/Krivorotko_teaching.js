11f33422f3fbb46bb7a41c6687f15bff5efbf870
Ilya Kabytov <kabytovi@gmail.com>
2024-09-11 12:46:42 +0700

    fix

--- src/Components/Krivorotko_teaching.js
+++ src/Components/Krivorotko_teaching.js
@@ -17,6 +17,15 @@ export function Krivorotko_teaching () {
         <div class="row my-4 ps-4"><p class="lead">Курс на <a rel="noopener" target="_blank" href="https://cytometry.ru/biomed/schedule.html">кафедре биомедицинской физики Новосибирского государственного университета</a> для магистрантов 5-6 курса.</p></div>
 <div class="row my-4 ps-4"><p class="lead">Авторы: <a rel="noopener" target="_blank" href="https://sites.google.com/site/yurkin/home">к.ф.-м.н. Юркин М.А.</a>, <a rel="noopener" target="_blank" href="/Krivorotko_info">д.ф.-м.н. Криворотько О.И.</a>.</p></div>
 <div class="row my-4 ps-4"><p class="lead">Сначала приведено краткое содержание, ниже - чуть подробнее расписана каждая тема. В подробном плане ссылки на названии темы ведут на презентации в формате PowerPoint. Доступна запись <a rel="noopener" target="_blank" href="https://youtube.com/playlist?list=PLPJ7DMh8UqNP9D98Urd5D1ZIzHTxVvb7j">лекций</a> (от 2023 г.). Основная литература доступна по <a rel="noopener" target="_blank" href="http://cyto.kinetics.nsc.ru/yurkin/InverseProblems/Literature/">ссылке</a>.</p></div>
+<div class="row my-4 ps-4"><h2>Введение</h2></div>
+<div class="row my-4 ps-4"><h4><a rel="noopener" target="_blank" href="https://covid19-modeling.ru/data/InvProb_01.pptx">1. Введение в курс</a></h4></div>
+<div class="row my-4 ps-4"><p class="lead">1.1. Цели и структура курса.</p></div>
+<div class="row my-4 ps-4"><p class="lead">1.2. Классификация обратных задач.</p></div>
+<div class="row my-4 ps-4"><p class="lead">1.3. Примеры обратных задач.</p></div>
+<div class="row my-4 ps-4"><p class="lead">1.4. Корректность обратной задачи: существование, единственность и устойчивость.</p></div>
+<div class="row my-4 ps-4"><p class="lead">1.5. Обсуждение конкретных примеров обратных задач в дипломных работах студентов.</p></div>
+<div class="row my-4 ps-4"><h2>Применение полученных знаний в дипломных работах студентов</h2></div>
+<div class="row my-4 ps-4"><p class="lead">В качестве экзаменационной работы студенты должны предоставить главу дипломной работы (для 5-го курса - предварительную), в которой описывается обработка экспериментальных данных. При проверке основное внимание будет уделяться на статистическую достоверность полученных результатов и выводов.</p></div>
 </section>
         </Container>
         
