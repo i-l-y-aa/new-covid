2585b9fdc5d65306abccb7f0b8076e081679ff98
Илья <kabytovi@gmail.com>
2024-12-24 20:54:47 +0700

    fix

--- src/Components/NaviBarv2.js
+++ src/Components/NaviBarv2.js
@@ -28,7 +28,6 @@ function NaviBarv2() {
         <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto" className="justify-content-end flex-grow-1 pe-4">
         <Nav.Link><NavLink to="/news" className={setActive} ><h5>Новости</h5></NavLink></Nav.Link>
-<<<<<<< HEAD
           <h5><NavDropdown title={<span className="fdrop">Научные направления</span>} id="collasible-nav-dropdown">
                       <NavDropdown.Item> <NavLink to="/data_processing_and_analysis" className={({isActive}) => isActive ? 'active-dropdown' : 'non-active-dropdown'} >Обработка и анализ данных</NavLink></NavDropdown.Item>
                       <NavDropdown.Item> <NavLink to="/the_spread_of_epidemics" className={({isActive}) => isActive ? 'active-dropdown' : 'non-active-dropdown'} >Моделирование и сценарии распространения эпидемий</NavLink></NavDropdown.Item>
@@ -36,9 +35,7 @@ function NaviBarv2() {
                       <NavDropdown.Item> <NavLink to="/pollution_modeling" className={({isActive}) => isActive ? 'active-dropdown' : 'non-active-dropdown'} >Моделирование загрязнений в регионах РФ</NavLink></NavDropdown.Item>
                       <NavDropdown.Item> <NavLink to="/medicine" className={({isActive}) => isActive ? 'active-dropdown' : 'non-active-dropdown'} >Моделирование в медицине</NavLink></NavDropdown.Item>
                     </NavDropdown></h5>
-=======
           <Nav.Link><NavLink to="/modeling" className={setActive}><h5>Моделирование</h5></NavLink></Nav.Link>
->>>>>>> 3f550d9aebe78ebf5ed5ca80909be387da0dd4db
           <h5><NavDropdown title={<span className="fdrop">Данные</span>} id="collasible-nav-dropdown">
             <NavDropdown.Item> <NavLink to="/data" className={({isActive}) => isActive ? 'active-dropdown' : 'non-active-dropdown'} >Данные для скачивания</NavLink></NavDropdown.Item>
             <NavDropdown.Item> <NavLink to="/links" className={({isActive}) => isActive ? 'active-dropdown' : 'non-active-dropdown'} >Полезные ссылки</NavLink></NavDropdown.Item>
